76cf762fa0d5b31f7027f1835168d3bb8f633309
Ivy <58640368+iivyleii@users.noreply.github.com>
2021-09-19 07:08:35 -0400

    more screens

--- lib/src/configtimer.dart
+++ lib/src/configtimer.dart
@@ -3,11 +3,14 @@ import 'package:flutter/material.dart';
 class MyForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    const appTitle = 'Form Styling Demo';
+    const appTitle = 'Pomodurance';
     return MaterialApp(
       title: appTitle,
       home: Scaffold(
+        backgroundColor: Color(0xFFECF7FE),
         appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Color(0xFFCA4A4A),
           title: const Text(appTitle),
         ),
         body: MyCustomForm(),
@@ -34,8 +37,11 @@ class MyCustomForm extends StatelessWidget {
             child: ElevatedButton(
               style: ElevatedButton.styleFrom(
                 shape: StadiumBorder(),
+                primary: Color(0xFFCA4A4A), // background
+                onPrimary: Color(0xFFFFFFF), // foreground
+                minimumSize: Size(200, 55),
               ),
-              child: Text('Ready!'),
+              child: Text('Ready!', style: TextStyle(color: Color(0xFFFFFFFF), fontSize: 30)),
               onPressed: () {},
             ),
           )
@@ -59,6 +65,8 @@ class singleForm extends StatelessWidget {
         width: size.width * 0.8,
         child: TextField(
           decoration: InputDecoration(
+            filled: true,
+            fillColor: Color(0xFF8AFFC1),
             border: OutlineInputBorder(),
             labelText: textvalue,
           ),
