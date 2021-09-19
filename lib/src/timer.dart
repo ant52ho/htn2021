c96a3fc42f9b70911dabbb1228bd66387aa5c8a6
Anthony Ho <anthony52ho@gmail.com>
2021-09-19 09:55:14 -0400

    added a timer

--- lib/src/timer.dart
+++ lib/src/timer.dart
@@ -3,21 +3,30 @@ import 'dart:async';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key = const Key("any_key")}) : super(key: key);
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 30;
+  int _counter = 1500;
+  String _display = "00:00";
   late Timer _timer;
 
+  String _printDuration(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2, "0");
+    String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
+    String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
+    return "${twoDigits(duration.inHours)}:$twoDigitMinutes:$twoDigitSeconds";
+  }
+
   void _startTimer() {
-    _counter = 30;
+    _counter = 1500;
     _timer = Timer.periodic(Duration(seconds: 1), (timer) {
       setState(() {
         if (_counter > 0) {
           _counter = _counter - 1;
+          var now = Duration(seconds: _counter);
+          _display = "${_printDuration(now)}";
         } else {
           _timer.cancel();
         }
@@ -45,13 +54,15 @@ class _MyHomePageState extends State<MyHomePage> {
                       fontSize: 48,
                     ),
                   ),
+            timerheading("WORK FOR"),
             Text(
-              '$_counter',
+              _display,
               style: TextStyle(
                 fontWeight: FontWeight.bold,
                 fontSize: 48,
               ),
             ),
+            timerheading("MINUTES!"),
             ElevatedButton(
               style: ElevatedButton.styleFrom(
                 shape: StadiumBorder(),
@@ -63,5 +74,25 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
     );
+  }
+}
+
+class timerheading extends StatelessWidget {
+  timerheading(this.textvalue);
+
+  final String textvalue;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return (Padding(
+      padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+      child: Text(
+        textvalue,
+        style: TextStyle(
+          fontSize: 24,
+        ),
+      ),
+    ));
   }
 }
